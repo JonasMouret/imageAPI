f1f3ee40811a569a6fb22c73e751cb0646147f77
jonas.mouret <jonas@Jonas-PC.localdomain>
2020-11-15 23:34:53 +0100

    Fix migration error

--- imageAPI/settings.py
+++ imageAPI/settings.py
@@ -74,6 +74,12 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 WSGI_APPLICATION = 'imageAPI.wsgi.application'
 
 
@@ -137,8 +143,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, "media")
+
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 django_heroku.settings(locals())
